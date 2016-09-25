24f074d8a52967e1f3dd38491a154943fe9b94f1
yeongwei <ywlai@origineit.com>
2016-09-25 16:08:39 +0800

    Solution submittion

--- app/src/main/java/mooc/vandy/java4android/shapes/logic/Logic.java
+++ app/src/main/java/mooc/vandy/java4android/shapes/logic/Logic.java
@@ -161,5 +161,45 @@ public class Logic
     }
 
     // TODO -- add your code here
+    public static double rectangleArea(double length, double width) {
+        return length * width;
+    }
+
+    public static double rectanglePerimeter(double length, double width) {
+        return 2.0 * (length + width);
+    }
+
+    public static double circleArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double circleCircumference(double radius) {
+        return 2.0 * Math.PI * radius;
+    }
+
+    public static double rightTriangleArea(double base, double height) {
+        return (1.0/2.0) * base * height;
+    }
+
+    public static double rightTrianglePerimeter(double base, double height) {
+        return base + height + Math.sqrt(Math.pow(base, 2) + Math.pow(height, 2));
+    }
+
+    public static double boxVolume(double length, double width, double depth) {
+        return length * width * depth;
+    }
+
+    public static double boxSurfaceArea(double length, double width,
+                                        double depth) {
+        return 2.0 * (length * width + width * depth + depth * length);
+    }
+
+    public static double sphereVolume(double radius) {
+        return (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+    }
+
+    public static double sphereSurfaceArea(double radius) {
+        return 4.0 * Math.PI * Math.pow(radius, 2);
+    }
 
 }
